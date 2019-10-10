82cbc30931bfe871c1571e1412f724f0378048df
Marco <marcocolu@hotmail.it>
2019-10-11 01:18:41 +0200

    Added popver for formatting and button
    Added all buttons, some are not displayed due to a missin feature in electron(i think), also icons not working, using special characters in label.

--- source/main/zettlr-touchbar.js
+++ source/main/zettlr-touchbar.js
@@ -1,29 +1,134 @@
 const { TouchBar } = require('electron')
-const { TouchBarLabel, TouchBarButton, TouchBarSpacer, TouchBarPopover, TouchBarColorPicker } = TouchBar
+const { TouchBarLabel, TouchBarButton, TouchBarSpacer, TouchBarPopover } = TouchBar
 
 const test = new TouchBarLabel()
+
 let touchBar = null
+let formattingBar = null
+
+let scrubber = null
+
+let hButton = null
 let boldButton = null
+let italicButton = null
+let linkButton = null
+let codeButton = null
+let commentButton = null
+let imageButton = null
+let numberedButton = null
+let itemizedButton = null
+let blockquoteButton = null
+
 class ZettlrTouchBar{
 	constructor(parent){
 		this._window = parent
 		test.label = 'testOk'
+		this.init()
+	}
+	
+	init () {
+		hButton = new TouchBarButton({
+  		label: '#',
+  		backgroundColor: '#367B66',
+  		click: () => {
+		    test.label = '#'
+  		}
+		})
+		
 		boldButton = new TouchBarButton({
-  		label: 'Bold',
+  		label: '𝗕',
   		backgroundColor: '#367B66',
   		click: () => {
 		    test.label = '****'
   		}
 		})
 		
+		italicButton = new TouchBarButton({
+  		label: '𝐼',
+  		backgroundColor: '#367B66',
+  		click: () => {
+		    test.label = '__'
+  		}
+		})
+		
+		codeButton = new TouchBarButton({
+  		label: '<>',
+  		backgroundColor: '#367B66',
+  		click: () => {
+		    test.label = '<>'
+  		}
+		})
+		
+		commentButton = new TouchBarButton({
+  		label: '</>',
+  		backgroundColor: '#367B66',
+  		click: () => {
+		    test.label = '</>'
+  		}
+		})
+		
+		linkButton = new TouchBarButton({
+  		label: '🔗',
+  		backgroundColor: '#367B66',
+  		click: () => {
+		    test.label = '[]()'
+  		}
+		})
+		
+		imageButton = new TouchBarButton({
+  		label: '🖼',
+  		backgroundColor: '#367B66',
+  		click: () => {
+		    test.label = '![]()'
+  		}
+		})
+		
+		blockquoteButton = new TouchBarButton({
+  		label: '❞',
+  		backgroundColor: '#367B66',
+  		click: () => {
+		    test.label = '\'\''
+  		}
+		})
+		
+		numberedButton = new TouchBarButton({
+  		label: '≡',
+  		backgroundColor: '#367B66',
+  		click: () => {
+		    test.label = '1.'
+  		}
+		})
+		
+		itemizedButton = new TouchBarButton({
+  		label: '≔',
+  		backgroundColor: '#367B66',
+  		click: () => {
+		    test.label = '*'
+  		}
+		})
+  	
+  	formattingBar = new TouchBar({
+  		items: [
+    		//test,
+    		hButton,
+    		boldButton,
+    		italicButton,
+    		codeButton,
+    		commentButton,
+    		linkButton,
+    		imageButton,
+    		numberedButton,
+    		itemizedButton,
+    		blockquoteButton
+  		]
+  	})
+				
 		touchBar = new TouchBar({
   		items: [
-    		test,
-    		boldButton
+    		new TouchBarPopover({label: 'Α', items: formattingBar, showCloseButton: false  })
   		]	
   	})
 	}
-	
 	
 	set () {
     //if macosOk
